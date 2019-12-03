623f0b7d4d37f4a4759c94d91a62f56b6140eecf
Nathan Sullivan <nsullivan@netsuite.com>
2019-12-03 01:34:16 -0800

    Add new function for more accurate fuel use
    Due to the tyranny of the rocket equation, the fuel to lift a
    module requires additional fuel to lift it as well.  The new
    module-fuel-required function starts with the fuel needed to
    lift the module mass, then iterates on the fuel required to
    lift each new block of fuel for lifting the fuel, until the
    fuel requirement is zero, then adds them all up to get the
    full fuel requirement for the module's mass.

--- problem1.clj
+++ problem1.clj
@@ -7,13 +7,17 @@
   [mass]
   (- (quot mass 3) 2))
 
+(defn module-fuel-required
+  [module-mass]
+  (reduce + 0 (take-while pos? (iterate fuel-required (fuel-required module-mass)))))
+
 (defn parse-input
   [filename]
   (map clojure.edn/read-string (clojure.string/split-lines (slurp filename))))
 
 (defn count-fuel
-  [masses]
-  (reduce + 0 (map fuel-required masses)))
+  [modules]
+  (reduce + 0 (map module-fuel-required modules)))
 
 (defn problem1
   []
