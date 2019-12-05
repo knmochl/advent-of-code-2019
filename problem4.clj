76a04f863425e73b5bd3aa087add88c8ba0ed215
Nathan Sullivan <nsullivan@netsuite.com>
2019-12-04 22:38:11 -0800

    Add code for Day 4 Part 2

--- problem4.clj
+++ problem4.clj
@@ -19,10 +19,22 @@
     (> (first numlist) (second numlist)) false
     :else (recur (rest numlist))))
 
+(defn pair-digit?
+  [numlist]
+  (pos? (count (filter #(= 2 %) (map count (partition-by identity numlist))))))
+
 (defn valid-pass?
   [numlist]
   (and (duplicate-digit? numlist) (ascending-digits? numlist)))
 
+(defn valid-pass2?
+  [numlist]
+  (and (duplicate-digit? numlist) (ascending-digits? numlist) (pair-digit? numlist)))
+
 (defn problem4-1
   []
   (count (filter (comp valid-pass? number-to-list) (range 372304 847061))))
+
+(defn problem4-2
+  []
+  (count (filter (comp valid-pass2? number-to-list) (range 372304 847061))))
