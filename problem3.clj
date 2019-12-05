6acbe074e6c9f177a22d617ddcfb0f72826d786a
Nathan Sullivan <nsullivan@netsuite.com>
2019-12-04 21:00:40 -0800

    Code for AOC Day 3 Part 1

---
+++ problem3.clj
@@ -0,0 +1,35 @@
+(ns advent.problem3)
+
+(defn wire-positions
+  [[start-x start-y] wire]
+  (let [direction (first wire)
+        distance (-> wire rest clojure.string/join Integer.)]
+    (cond
+      (= direction \R)
+      (for [x (range (inc start-x) (+ start-x (inc distance)))] [x start-y])
+      (= direction \L)
+      (for [x (range (dec start-x) (- start-x (inc distance)) -1)] [x start-y])
+      (= direction \U)
+      (for [y (range (inc start-y) (+ start-y (inc distance)))] [start-x y])
+      (= direction \D)
+      (for [y (range (dec start-y) (- start-y (inc distance)) -1)] [start-x y]))))
+
+(defn run-wires
+  ([wires] (run-wires wires [[0 0]]))
+  ([wires positions]
+   (if (empty? wires)
+     positions
+     (recur (rest wires) (concat positions (wire-positions (last positions) (first wires)))))))
+
+(defn load-wires
+  [filename]
+  (map #(clojure.string/split % #",") (-> filename slurp clojure.string/split-lines)))
+
+(defn find-intersections
+  [wire-list]
+  (apply clojure.set/intersection (map #(set (drop 1 (run-wires %))) wire-list)))
+
+(defn problem3
+  []
+  (first (sort (map #(+ (Math/abs (first %)) (Math/abs (second %))) (find-intersections (load-wires "input3.txt"))))))
+
