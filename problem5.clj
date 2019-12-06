fd89c7517bee4a23df6defa570cf40fcde2b3757
Nathan Sullivan <nsullivan@netsuite.com>
2019-12-06 01:32:28 -0800

    Add code for Part 2 of Day 5
    Interestingly, the same program code is used, even though part 2
    required new opcodes.  The only change between the two parts is the
    value on the input.  Quite clever code, apparently.

--- problem5.clj
+++ problem5.clj
@@ -6,3 +6,7 @@
   []
   (nth (advent.intcode/run-opcode (advent.intcode/load-program "input5.txt") 0 [1] []) 3))
 
+(defn problem5-2
+  []
+  (nth (advent.intcode/run-opcode (advent.intcode/load-program "input5.txt") 0 [5] []) 3))
+
