32434522c8d2a888a7fb6a81e2b04c55dc4ce8fa
Nathan Sullivan <nsullivan@netsuite.com>
2019-12-10 00:32:07 -0800

    Fixes for Intcode computer
    Typo in load-program
    
    Misunderstood logic for opcode 9, the value is added to the current
    base, not replaces it
    
    Mode 2 is valid for target addresses, but also works a little
    differently than other values.  Regular values would use the
    relative address to point to the place to read for the value.
    For targets, the relative address is the "value".  Added a
    get-target function and changed all target lookups on opcodes to
    use it.  It treats mode 0 and 1 as the same, and does relative
    address for mode 2.

--- src/advent/intcode.clj
+++ src/advent/intcode.clj
@@ -16,6 +16,15 @@
       (= mode 2)
       (get-value machine (+ (:relative machine) mem-val) 1))))
 
+(defn get-target
+  [machine pos mode]
+  (let [memory (:memory machine)
+        mem-val (get memory pos 0)]
+    (cond
+      (= mode 2)
+      (+ (:relative machine) mem-val)
+      :else mem-val)))
+
 (defn set-value
   [memory pos new-value]
   (let [mem-size (count memory)]
@@ -51,19 +60,19 @@
       (= opcode 1)
       (let [value1 (get-value machine (+ instruction-pointer 1) (get-mode modes 1))
             value2 (get-value machine (+ instruction-pointer 2) (get-mode modes 2))
-            target (get-value machine (+ instruction-pointer 3) 1)
+            target (get-target machine (+ instruction-pointer 3) (get-mode modes 3))
             new-memory (set-value memory target (+ value1 value2))
             new-ip (+ instruction-pointer 4)]
         (assoc machine :memory new-memory :ip new-ip))
       (= opcode 2)
       (let [value1 (get-value machine (+ instruction-pointer 1) (get-mode modes 1))
             value2 (get-value machine (+ instruction-pointer 2) (get-mode modes 2))
-            target (get-value machine (+ instruction-pointer 3) 1)
+            target (get-target machine (+ instruction-pointer 3) (get-mode modes 3))
             new-memory (set-value memory target (* value1 value2))
             new-ip (+ instruction-pointer 4)]
         (assoc machine :memory new-memory :ip new-ip))
       (= opcode 3)
-      (let [target (get-value machine (+ instruction-pointer 1) 1)
+      (let [target (get-target machine (+ instruction-pointer 1) (get-mode modes 1))
             input (:input machine)
             new-memory (set-value memory target (first input))
             new-input (rest input)
@@ -90,7 +99,7 @@
       (= opcode 7)
       (let [value1 (get-value machine (+ instruction-pointer 1) (get-mode modes 1))
             value2 (get-value machine (+ instruction-pointer 2) (get-mode modes 2))
-            target (get-value machine (+ instruction-pointer 3) 1)
+            target (get-target machine (+ instruction-pointer 3) (get-mode modes 3))
             new-ip (+ instruction-pointer 4)
             new-memory (if (< value1 value2)
                          (set-value memory target 1)
@@ -99,15 +108,16 @@
       (= opcode 8)
       (let [value1 (get-value machine (+ instruction-pointer 1) (get-mode modes 1))
             value2 (get-value machine (+ instruction-pointer 2) (get-mode modes 2))
-            target (get-value machine (+ instruction-pointer 3) 1)
+            target (get-target machine (+ instruction-pointer 3) (get-mode modes 3))
             new-ip (+ instruction-pointer 4)
             new-memory (if (= value1 value2)
                          (set-value memory target 1)
                          (set-value memory target 0))]
         (assoc machine :memory new-memory :ip new-ip))
       (= opcode 9)
-      (let [value1 (get-value machine (+ instruction-pointer 1) (get-mode modes 1))]
-        (assoc machine :ip (+ instruction-pointer 2) :relative value1))
+      (let [value1 (get-value machine (+ instruction-pointer 1) (get-mode modes 1))
+            relative-base (:relative machine)]
+        (assoc machine :ip (+ instruction-pointer 2) :relative (+ value1 relative-base)))
       :else "error")))
 
 (defn run-to-output
@@ -129,7 +139,7 @@
 
 (defn load-program
   [filename]
-  (vec (map #(edn/read-string) (str/split (str/trim-newline (slurp (jio/resource filename))) #","))))
+  (vec (map edn/read-string (str/split (str/trim-newline (slurp (jio/resource filename))) #","))))
 
 (defn make-machine
   ([memory]
