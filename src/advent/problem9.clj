9c7365f3eaf56aafcbaf9b2d77cf68e7be0a35c4
Nathan Sullivan <nsullivan@netsuite.com>
2019-12-10 00:36:00 -0800

    Add solution for Part 2 of Day 9

--- src/advent/problem9.clj
+++ src/advent/problem9.clj
@@ -4,3 +4,7 @@
 (defn problem9-1
   []
   (:output (intcode/run-opcode (intcode/make-machine (intcode/load-program "input9.txt") [1]))))
+
+(defn problem9-2
+  []
+  (:output (intcode/run-opcode (intcode/make-machine (intcode/load-program "input9.txt") [2]))))
