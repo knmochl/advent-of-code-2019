862191e103ca01cc4af02b42e9495ddfc41f4dc8
Nathan Sullivan <nsullivan@netsuite.com>
2020-03-25 21:12:40 -0700

    Add lanterna dep and main function
    For day 13 part 2, it helps to have a visual display.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.async "0.6.532"]]
+                 [org.clojure/core.async "0.6.532"]
+                 [clojure-lanterna "0.9.7"]]
   :main ^:skip-aot advent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/advent/core.clj
@@ -0,0 +1,8 @@
+(ns advent.core
+  (:require [lanterna.screen :as s])
+  (:use [advent.problem13 :only [problem13-1 problem13-2]]))
+
+(defn -main
+  []
+  (println (problem13-2)))
+
