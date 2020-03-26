a27b8e604748f04ee38f08a88b74b1ddc59b515c
Nathan Sullivan <nsullivan@netsuite.com>
2020-03-25 21:14:58 -0700

    Add visual and fix control flow
    Use the control channel to manage inputs, and fix up the logic.
    Also add a visual display of the game.

--- src/advent/problem13.clj
+++ src/advent/problem13.clj
@@ -1,6 +1,14 @@
 (ns advent.problem13
-  (:require [clojure.core.async :as async :refer [<!! >!!]]
+  (:require [clojure.core.async :as async :refer [<!! >!! alts!! chan]]
+            [lanterna.screen :as s]
             [advent.intcode :as intcode]))
+
+(def glyphs
+  {0 "."
+   1 "#"
+   2 "X"
+   3 "="
+   4 "O"})
 
 (defn remove-tile
   [tiles x y]
@@ -12,9 +20,9 @@
 
 (defn process-tile
   [tiles x y kind]
-  (case kind
-    0 (remove-tile tiles x y)
-    (1 2 3 4) (add-or-replace-tile tiles x y kind)))
+  (if (= kind 0)
+    (remove-tile tiles x y)
+    (add-or-replace-tile tiles x y kind)))
 
 (defn run-game
   [machine tiles]
@@ -25,9 +33,56 @@
           kind (<!! (:output machine))]
       (recur machine (process-tile tiles x y kind))))))
 
+(defn draw-screen
+  [screen tiles]
+  (s/clear screen)
+  (doseq [[x y :as key] (keys tiles)]
+    (if (and (= x -1) (= y 0))
+      (s/put-string screen 0 0 (str (get tiles key)))
+      (s/put-string screen x (inc y) (get glyphs (get tiles key) "?"))))
+  (s/redraw screen))
+
 (defn problem13-1
   []
   (let [machine (intcode/make-machine (intcode/load-program "input13.txt"))
         run (intcode/execute-machine machine)
+        screen (s/get-screen :swing)
         tiles (run-game machine {})]
-    (count (filter #(= 2 (second %)) tiles))))
+    (do 
+      (s/start screen)
+      (draw-screen screen tiles)
+      (s/get-key-blocking screen)
+      (s/stop screen)
+      (count (filter #(= 2 (second %)) tiles)))))
+
+(defn play-game
+  [machine tiles screen ball paddle]
+  (let [machine-output (:output machine)
+        machine-control (:control machine)
+        [x channel] (alts!! [machine-output machine-control])]
+      (if (= channel machine-control)
+        (do
+          (println "Send input")
+          (>!! (:input machine) (compare (first ball) (first paddle)))
+          (println (str (first ball) " " (first paddle) " " (compare (first ball) (first paddle))))
+          (draw-screen screen tiles)
+          (recur machine tiles screen ball paddle))
+        (let [y (<!! machine-output)
+              kind (<!! machine-output)
+              new-tiles (process-tile tiles x y kind)]
+          (case kind
+            (0 1 2) (recur machine new-tiles screen ball paddle)
+            3 (recur machine new-tiles screen ball [x y])
+            4 (recur machine new-tiles screen [x y] paddle)
+            nil (do (s/get-key-blocking screen)
+                    (get new-tiles [-1 0]))
+            (recur machine new-tiles screen ball paddle))))))
+
+(defn problem13-2
+  []
+  (let [input-chan (chan 3)
+        output-chan (chan 3)
+        machine (intcode/make-machine (assoc (intcode/load-program "input13.txt") 0 2))
+        screen (s/get-screen :swing)]
+    (intcode/execute-machine machine)
+    (s/in-screen screen (play-game machine {} screen nil nil))))
